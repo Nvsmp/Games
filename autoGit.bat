4630d08814e81d7198f33b076e3380e746f84494
Nvsmp <oldblack26@gmail.com>
2023-05-25 22:00:48 -0300

    Comitestei2

--- autoGit.bat
+++
@@ -1,7 +0,0 @@
-git init
-git config --global user.name "Nvsmp"
-git config --global user.email "oldblack26@gmail.com"
-git config --global http.proxy 10.1.3.1:4239
-git remote add games https://github.com/Nvsmp/Games.git
-git pull games
-git checkout main
