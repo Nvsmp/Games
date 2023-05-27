6fa654dd0aa6c6f1c492dd696095a883d0f4c250
Nvsmp <oldblack26@gmail.com>
2023-05-27 14:28:35 -0300

    escudo

--- Space 1.2/main.py
+++ Space 1.2/main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys,os,time
+from pygame.locals import *
 
 pygame.init()
 
@@ -11,14 +12,14 @@ def update_laser(laser_list):
         if laserRec.midbottom[1] < 0:
             laser_list.remove(laserRec)
 #Pontuacao
-def displayScore(tela, font):
+def display_score(tela, font):
     score_text = str(f'S T A R - G A M E {pygame.time.get_ticks()//1000}')
     texto = font.render(score_text, True, (255,255,255))
     recText = texto.get_rect(midleft=(30,15))
     tela.blit(texto, recText)
 
 #Tela
-pygame.display.set_caption('<- A COBRINHA AKI Q FOFA')
+pygame.display.set_caption('ACOBRINHA AKI')
 largura, altura = 1280,720 #FULL HD
 tela = pygame.display.set_mode((largura, altura))
 
@@ -61,11 +62,15 @@ naveRec = nave.get_rect(center = (500,500))
 
 
 while(loop):
+
     dt = relogio.tick(120)/1000
     naveRec.center = pygame.mouse.get_pos()
+    mousex ,mousey = pygame.mouse.get_pos()
+
     for event in pygame.event.get():
+
         if event.type == pygame.MOUSEBUTTONDOWN:
-            laserRec = lasersurf.get_rect(midbottom=naveRec.midtop)
+            laserRec = lasersurf.get_rect(midbottom=naveRec.midtop) #?
             laser_list.append(laserRec)
 
         if event.type == pygame.MOUSEBUTTONUP:
@@ -74,6 +79,8 @@ while(loop):
         if event.type == pygame.QUIT:
             loop = False
             sys.exit()
+
+        #EVENTO MOVIMENTO (KEYDOWN e KEYUP) | n funciona enquanto posicao do naveRec.center seguir o mouse
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 mov_esquerda = True
@@ -92,8 +99,6 @@ while(loop):
                 mov_cima = False
             if event.key == pygame.K_s:
                 mov_baixo = False
-
-
     if mov_esquerda:
         naveRec.x -= velocidade
     if mov_direita:
@@ -103,12 +108,10 @@ while(loop):
     if mov_baixo:
         naveRec.y += velocidade
 
-
-    start = int(round(time.time()*1000))
     tela.blit(fundo, (0,0))
     tela.blit(nave, naveRec)
     #tela.blit(texto,bgR1)
-    displayScore(tela=tela, font=font)
+    display_score(tela=tela, font=font)
     #laserRec.y -= velocidadeDisparo
     #tela.blit(lasersurf,laserRec)
     if naveRec.y <= 0:
@@ -121,15 +124,20 @@ while(loop):
         naveRec.x = 0
 
     update_laser(laser_list)
+
+    surfaceEscudo = pygame.Surface((200, 200), pygame.SRCALPHA)
+    pygame.draw.circle(surfaceEscudo, (200, 200, 200, 100), (100, 100), 50)
+    tela.blit(surfaceEscudo, (mousex -100, mousey - 100))
+    #pygame.display.flip()
+    #pygame.time.Clock().tick(60)
+
     for laserRec in laser_list:
         tela.blit(lasersurf,laserRec)
         print(laser_list)
-    
-    pygame.display.update()
-    end = int(round(time.time()*1000))
-    
+
+    #start = int(round(time.time() * 1000))
+    #end = int(round(time.time()*1000))    Pega o "ping" entre as cenas
     #print(f'{end-start} ms')
 
-
-
+    pygame.display.update()
 pygame.quit()
